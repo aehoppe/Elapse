317c4b5e0226a35f9b7c2683ffadd88330b3d6f4
Gaby Clarke <gabyclarke@me.com>
2016-04-19 19:46:50 -0400

    icalFile request not working

--- app/views.py
+++ app/views.py
@@ -8,44 +8,34 @@ from app import app
 from flask import request, render_template
 from werkzeug import secure_filename
 from flask_wtf import Form
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+# from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+icalFile = 'test'
 
 # INDEX APP ROUTES
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
-
-# class icalForm(Form):
-#     """ ical file upload form """
-#     icalFile = FileField('Your ical', validators=[
-#         FileRequired(),
-#         FileAllowed(['ics'], '.ics files only')
-#     ])
-
+    return render_template('index.html')
 
 # ICAL UPLOAD PAGE
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
-    # form = icalForm()
-    # if form.validate_on_submit():
-    #     filename = secure_filename(form.icalFile.data.filename)
-    #     form.icalFile.data.save('uploads/' + filename)
-    # else:
-    #     filename = None
+    if request.method == 'POST':
+        global icalFile
+        print request.form['icalFile']
+        icalFile = request.form['icalFile']
+        print icalFile
+    return render_template('upload.html')
 
-    icalFile = request.form['icalFile']
-    
-    return render_template('upload.html', form=form, filename=filename)
-
+# EDIT CALENDAR EVENTS
+@app.route('/edit', methods=['POST', 'GET'])
+def edit():
+    global icalFile
+    print icalFile
+    return render_template('edit.html', events=icalFile.events)
 
 # VIS1 VIEW PAGE
 @app.route('/vis1')
 def vis1():
-    return render_template('templates/vis1.html')
-
-# edit calendar event tags
-@app.route('/edit')
-def edit():
-    return render_template('templates/edit.html', events=cal.events)
+    return render_template('vis1.html')
